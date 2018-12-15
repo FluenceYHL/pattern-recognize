3fde63c4861be4f5e4429173cd618ab92422dd42
FluenceYHL <37389680+FluenceYHL@users.noreply.github.com>
2018-12-15 16:18:14 +0800

    Update bayers_plot.py

--- Homework/1/bayers_plot.py
+++ Homework/1/bayers_plot.py
@@ -46,21 +46,17 @@ class Bayers():
         self.inv_boys = numpy.linalg.inv(self.cov_boys)
         self.det_boys = numpy.linalg.det(self.cov_boys)
 
-    def predict(self, inpt):
+    def predict(self, inpt, rate=0.5):
         lhs = -0.5 * ((inpt - self.mean_girls).T.dot(self.inv_girls).dot(inpt -
-                                                                         self.mean_girls) + math.log(self.det_girls)) + math.log(0.2)
+                                                                         self.mean_girls) + math.log(self.det_girls)) + math.log(rate)
         rhs = -0.5 * ((inpt - self.mean_boys).T.dot(self.inv_boys).dot(inpt -
-                                                                       self.mean_boys) + math.log(self.det_boys)) + math.log(0.8)
+                                                                       self.mean_boys) + math.log(self.det_boys)) + math.log(1 - rate)
         return 0 if(lhs > rhs) else 1
 
     def predict_all(self, inpt):
         res = []
-        cnt = 0
         for it in inpt:
             res.append(self.predict(it))
-            if(cnt % 10000 == 0):
-                print(str(cnt / 10000) + ' is over.....')
-            cnt = cnt + 1
         return numpy.array(res)
 
     def score(self, x, y):
@@ -72,6 +68,7 @@ class Bayers():
                 correct = correct + 1
             cnt = cnt + 1
         print('正确率　　' + str(correct / len(x)))
+        return correct / len(x)
 
 
 if __name__ == '__main__':
@@ -79,6 +76,11 @@ if __name__ == '__main__':
     boys = getData('../dataSet/male.txt')
     one = Bayers()
     one.fit(girls, boys)
+    # x = numpy.r_[girls, boys]
+    # y = [0] * len(girls)
+    # for it in boys:
+    #     y.append(1)
+
     x, y = getTest()
     one.score(x, y)
 
@@ -87,7 +89,6 @@ if __name__ == '__main__':
     # 获取边界范围, 为了产生数据
     x1_min, x1_max = numpy.min(x[:, 0]) - 1, numpy.max(x[:, 0]) + 1
     x2_min, x2_max = numpy.min(x[:, 1]) - 1, numpy.max(x[:, 1]) + 1
-
     # 生成新的数据, 并调用meshgrid网格搜索函数帮助我们生成矩阵
     xx1, xx2 = numpy.meshgrid(numpy.arange(
         x1_min, x1_max, 0.1), numpy.arange(x2_min, x2_max, 0.1))
@@ -106,3 +107,37 @@ if __name__ == '__main__':
     plt.ylabel('体重')
     plt.savefig('Bayers 分类器决策边界.png')
     plt.show()
+
+    # girls = getData('../dataSet/female.txt')
+    # boys = getData('../dataSet/male.txt')
+    # one = Bayers()
+    # one.fit(girls, boys)
+    # x = numpy.r_[girls, boys]
+    # y = [0] * len(girls)
+    # for it in boys:
+    #     y.append(1)
+
+    # # x, y = getTest()
+    # # one.score(x, y)
+
+    # cmap_light = ListedColormap(['#AAAAFF', '#AAFFAA', '#FFAAAA'])
+    # ori_light = ListedColormap(['r', 'g'])
+    # # 获取边界范围, 为了产生数据
+    # x1_min, x1_max = numpy.min(x[:, 0]) - 1, numpy.max(x[:, 0]) + 1
+    # x2_min, x2_max = numpy.min(x[:, 1]) - 1, numpy.max(x[:, 1]) + 1
+    # # 生成新的数据, 并调用meshgrid网格搜索函数帮助我们生成矩阵
+    # xx1, xx2 = numpy.meshgrid(numpy.arange(
+    #     x1_min, x1_max, 0.1), numpy.arange(x2_min, x2_max, 0.1))
+    # # 有了新的数据, 我们需要将这些数据输入到分类器获取到结果, 但是因为输入的是矩阵, 我们需要给你将其转换为符合条件的数据
+    # Z = one.predict_all(numpy.c_[xx1.ravel(), xx2.ravel()])
+    # # 这个时候得到的是Z还是一个向量, 将这个向量转为矩阵即可
+    # Z = Z.reshape(xx1.shape)
+    # plt.figure()
+    # plt.pcolormesh(xx1, xx2, Z, cmap=cmap_light)
+    # x = numpy.r_[girls, boys]
+    # plt.scatter(x[:, 0], x[:, 1], c=y, cmap=ori_light)
+    # plt.title('Bayers 分类器决策边界')
+    # plt.xlabel('身高')
+    # plt.ylabel('体重')
+    # plt.savefig('Bayers 分类器决策边界.png')
+    # plt.show()
